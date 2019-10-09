6d179f2da0525f0c9fdcfdbc1181db5d61be7456
darrylceguerra@gmail.com <darrylceguerra@gmail.com>
2019-10-09 15:41:22 +0800

    added quasar

---
+++ src/quasar.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+
+import './static/styles/quasar.styl'
+import 'quasar/dist/quasar.ie.polyfills'
+import '@quasar/extras/material-icons/material-icons.css'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}
+ })
