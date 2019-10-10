48b86d4a8639f92ad0fd6ec1e91d18da390f8fbe
darrylceguerra@gmail.com <darrylceguerra@gmail.com>
2019-10-10 10:31:56 +0800

    added quasar

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './quasar'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import router from './router';
+import store from './store';
+
+import './quasar';
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	router,
+	store,
+	render: h => h(App)
+}).$mount('#app');
